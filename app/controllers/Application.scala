8ee465388cff8259e41f8b18a91c3b0484559863
James Owers <jamesowers@gmail.com>
2015-08-03 08:17:19 +0100

    Got form, form validation and success page working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,13 +1,33 @@
 package controllers
 
+import play.api.Play.current
+import play.api.data.Form
+import play.api.data.Forms._
+
 import models.User
 
 import play.api.mvc.{ Action, Controller }
 
 object Application extends Controller {
 
+  val userForm = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "email" -> email
+    )(User.apply)(User.unapply)
+  )
+
   def index = Action {
-    Ok("Hello")
+    Ok(views.html.form(userForm))
+  }
+
+  def saveUser = Action{ implicit request =>
+    userForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html.form(formWithErrors)),
+      success => {
+        Ok(views.html.success())
+      }
+    )
   }
 
 }
